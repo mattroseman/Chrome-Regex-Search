745dd977a45367d30ca8fa7053fc1add006f64e5
Matthew Roseman <mrr77@case.edu>
2016-03-29 12:17:20 -0400

    added matchpage js file that is tasked with searching a page for the regex expression
    Signed-off-by: Matthew Roseman <mrr77@case.edu>

---
+++ src/matchpage.js
@@ -0,0 +1,7 @@
+/*jslint browser: true*/
+/*global $, JQuery, alert, chrome*/
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+
+    });
--- src/popup.js
+++ src/popup.js
@@ -1,12 +1,32 @@
 /*jslint browser: true*/
-/*global $, jQuery, alert*/
+/*global $, jQuery, alert, getInputText*/
+
+
+var regex; // the regular expression being searched
 
 $(document).ready(function() {
 
     console.log("popup.js started");
 
+
+    // button next is pressed
     $("#next").click(function () {
-        var regex = $("#regex").val();
-        console.log(regex);
+        getInputText();
+    });
+
+    $("#prev").click(function () {
+        getInputText();
     });
 });
+
+/**
+ * Grabs text form the search textbox and assigns it to regex 
+ * @return returns the new regex value gotten from the textbox id="regex"
+ */
+function getInputText() {
+    // get input text from #regex and assign it to regex
+    regex = $("#regex").val();
+    // keep the text in the textbox the same
+    $("#regex").val(regex);
+    return regex;
+}
